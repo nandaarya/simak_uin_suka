1b25bdbaddae9ff638de82bfe650467a28abf551
Nanda Arya Putra <nandaarya404@gmail.com>
2023-04-09 14:57:09 +0700

    add bottom navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,19 @@
 import 'package:flutter/material.dart';
 // import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/services.dart';
+import 'package:simak_uin_suka/view/main_page.dart';
+
 
 void main() async {
   // WidgetsFlutterBinding.ensureInitialized();
   // await EasyLocalization.ensureInitialized();
 
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     SystemChrome.setPreferredOrientations([
@@ -21,6 +25,7 @@ class MyApp extends StatelessWidget {
           primarySwatch: Colors.orange,
         ),
         debugShowCheckedModeBanner: false,
+        home: const MainPage(),
         // initialRoute: '/',
         // routes: {
         //   '/': (context) => SplashPage(),
---
+++ lib/view/main_page.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:convex_bottom_bar/convex_bottom_bar.dart';
+import 'package:flutter/services.dart';
+import 'home_page.dart';
+import 'profile_page.dart';
+import 'package:simak_uin_suka/theme.dart';
+
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  int currentIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    List<Widget> widgets = [
+      const HomePage(),
+      const ProfilePage(),
+    ];
+
+    return WillPopScope(
+      onWillPop: () async {
+        SystemChannels.platform.invokeMethod('SystemNavigator.pop');
+        return false;
+      },
+      child: Scaffold(
+        body: widgets[currentIndex],
+        bottomNavigationBar: ConvexAppBar(
+            style: TabStyle.textIn,
+            backgroundColor: primaryColor,
+            items: const [
+              TabItem(icon: Icons.home, title: 'Beranda'),
+              TabItem(icon: Icons.person, title: 'Profil'),
+            ],
+            initialActiveIndex: 0,
+            onTap: (int i) {
+              setState(() {
+                currentIndex = i;
+              });
+            }),
+      ),
+    );
+  }
+}
