4891b312b320787e20b7eb2e7292645cefccc5a8
Nanda Arya Putra <nandaarya404@gmail.com>
2023-04-11 23:02:48 +0700

    add profile page

--- lib/view/profile_page.dart
+++ lib/view/profile_page.dart
@@ -12,6 +12,9 @@ class _ProfilePageState extends State<ProfilePage> {
   @override
   Widget build(BuildContext context) {
 
+    double widthDevice = MediaQuery.of(context).size.width;
+    double heightDevice = MediaQuery.of(context).size.height - 50;
+
     Widget header() {
       return Container(
         alignment: AlignmentDirectional.centerStart,
@@ -30,11 +33,48 @@ class _ProfilePageState extends State<ProfilePage> {
         ),
       );
     }
+    
+    Widget profile() {
+      return Container(
+        height: 200,
+        padding: EdgeInsets.all(defaultPadding),
+        margin: EdgeInsets.symmetric(horizontal: defaultMargin),
+        decoration: BoxDecoration(
+          color: primaryColor,
+          borderRadius: BorderRadius.circular(12)
+        ),
+        child: Column(
+          children: [
+            Icon(Icons.account_circle, size: 100,),
+            Text('Nanda Arya Putra', style: h2,),
+            Text('21106050048', style: h2,),
+            Text('User Mahasiswa', style: h2,)
+          ],
+        ),
+      );
+    }
+
+    Widget setting() {
+      return Container(
+        height: 150,
+        margin: EdgeInsets.symmetric(horizontal: defaultMargin),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text('Ubah Bahasa', style: h2,),
+            Text('Ubah Data', style: h2,),
+            Text('Ubah Kata Sandi', style: h2,),
+            Text('Logout', style: h2.copyWith(color: Colors.red),)
+          ],
+        ),
+      );
+    }
 
     return Scaffold(
       body: ListView(
-        physics: NeverScrollableScrollPhysics(),
-        children: [header(), const SizedBox(height: 24)],
+        // physics: NeverScrollableScrollPhysics(),
+        children: [header(), const SizedBox(height: 24), profile(), const SizedBox(height: 24), setting()],
       ),
     );
   }
