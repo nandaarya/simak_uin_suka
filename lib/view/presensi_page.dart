15f05beb10e7030c180eb1d4d231f18339c844ee
Nanda Arya Putra <nandaarya404@gmail.com>
2023-04-16 23:55:12 +0700

    add presensi page

---
+++ lib/view/presensi_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PresensiPage extends StatefulWidget {
+  const PresensiPage({Key? key}) : super(key: key);
+
+  @override
+  State<PresensiPage> createState() => _PresensiPageState();
+}
+
+class _PresensiPageState extends State<PresensiPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
