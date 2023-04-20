4acf7999ccaccee782b26f2ec02fb9eb8489f017
Nanda Arya Putra <nandaarya404@gmail.com>
2023-04-21 00:19:16 +0700

    add Sign In Page

---
+++ lib/view/sign_in_page.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:flutter_login/flutter_login.dart';
+import 'package:simak_uin_suka/view/main_page.dart';
+
+const users = const {
+  'akunpribadi413@gmail.com': '12345',
+  'hunter@gmail.com': 'hunter',
+};
+
+class SignInPage extends StatelessWidget {
+  Duration get loginTime => Duration(milliseconds: 2250);
+
+  Future<String?> _authUser(LoginData data) {
+    debugPrint('Name: ${data.name}, Password: ${data.password}');
+    return Future.delayed(loginTime).then((_) {
+      if (!users.containsKey(data.name)) {
+        return 'User not exists';
+      }
+      if (users[data.name] != data.password) {
+        return 'Password does not match';
+      }
+      return null;
+    });
+  }
+
+  Future<String?> _signupUser(SignupData data) {
+    debugPrint('Signup Name: ${data.name}, Password: ${data.password}');
+    return Future.delayed(loginTime).then((_) {
+      return null;
+    });
+  }
+
+  Future<String?> _recoverPassword(String name) {
+    debugPrint('Name: $name');
+    return Future.delayed(loginTime).then((_) {
+      if (!users.containsKey(name)) {
+        return 'User not exists';
+      }
+      return null;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FlutterLogin(
+      title: 'Login',
+      // logo: AssetImage('assets/images/logo_uin.png'),
+      onLogin: _authUser,
+      onSignup: _signupUser,
+
+      loginProviders: <LoginProvider>[
+        LoginProvider(
+          icon: FontAwesomeIcons.google,
+          label: 'Google',
+          callback: () async {
+            debugPrint('start google sign in');
+            await Future.delayed(loginTime);
+            debugPrint('stop google sign in');
+            return null;
+          },
+        ),
+      ],
+      onSubmitAnimationCompleted: () {
+        Navigator.of(context).pushReplacement(MaterialPageRoute(
+          builder: (context) => MainPage(),
+        ));
+      },
+      onRecoverPassword: _recoverPassword,
+    );
+  }
+}
--- lib/view/splash_page.dart
+++ lib/view/splash_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:easy_splash_screen/easy_splash_screen.dart';
 import 'package:simak_uin_suka/theme.dart';
-import 'package:simak_uin_suka/view/main_page.dart';
+import 'package:simak_uin_suka/view/sign_in_page.dart';
 
 class SplashPage extends StatefulWidget {
   const SplashPage({Key? key}) : super(key: key);
@@ -29,7 +29,7 @@ class _SplashPageState extends State<SplashPage> {
       showLoader: true,
       loadingText: Text("Mengunduh Data...", style: h2,),
       // futureNavigator: futureCall(),
-      navigator: const MainPage(),
+      navigator: SignInPage(),
     );
   }
 }
