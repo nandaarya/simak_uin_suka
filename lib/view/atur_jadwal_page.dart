0c0fc0f143b3d0108883ed7b86da6240698105f8
Nanda Arya Putra <nandaarya404@gmail.com>
2023-04-29 20:31:54 +0700

    add widget

--- lib/view/atur_jadwal_page.dart
+++ lib/view/atur_jadwal_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:simak_uin_suka/theme.dart';
-// import 'package:http/http.dart' as http;
-// import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class AturJadwalPage extends StatefulWidget {
   const AturJadwalPage({Key? key}) : super(key: key);
@@ -11,25 +11,25 @@ class AturJadwalPage extends StatefulWidget {
 }
 
 class _AturJadwalPageState extends State<AturJadwalPage> {
-  // List kelas = []; //untuk menampung data dari API
-  // String? selectedValue; //untuk menyimpan nilai yang dipilih
-  //
-  //
-  // Future<String> getDataKelas() async {
-  //   var response = await http.get(
-  //       Uri.parse("https://dummyjson.com/products/1")); //ubah URL dengan URL API yang ingin diambil datanya
-  //   setState(() {
-  //     var dataJSON = json.decode(response.body);
-  //     kelas = dataJSON["products"]; //ubah "items" dengan key atau nama array yang menyimpan data dari API
-  //   });
-  //   return "Success!";
-  // }
-  //
-  // @override
-  // void initState() {
-  //   super.initState();
-  //   getDataKelas();
-  // }
+  List kelas = []; //untuk menampung data dari API
+  String? selectedValue; //untuk menyimpan nilai yang dipilih
+
+
+  Future<String> getDataKelas() async {
+    var response = await http.get(
+        Uri.parse("https://dummyjson.com/products/1")); //ubah URL dengan URL API yang ingin diambil datanya
+    setState(() {
+      var dataJSON = json.decode(response.body);
+      kelas = dataJSON["products"]; //ubah "items" dengan key atau nama array yang menyimpan data dari API
+    });
+    return "Success!";
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getDataKelas();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -72,23 +72,23 @@ class _AturJadwalPageState extends State<AturJadwalPage> {
       );
     }
 
-    // Widget daftarkelas() {
-    //   return DropdownButton(
-    //     hint: Text("Select item"), //text yang muncul pada DropdownButton ketika belum ada item yang dipilih
-    //     value: selectedValue,
-    //     onChanged: (newValue) {
-    //       setState(() {
-    //         selectedValue = newValue;
-    //       });
-    //     },
-    //     items: kelas.map((item) {
-    //       return DropdownMenuItem(
-    //         value: item['id'].toString(),
-    //         child: Text(item['title']), //ubah "id" dengan key atau nama yang ingin dijadikan nilai pada DropdownMenuItem
-    //       );
-    //     }).toList(),
-    //   );
-    // }
+    Widget daftarkelas() {
+      return DropdownButton(
+        hint: Text("Select item"), //text yang muncul pada DropdownButton ketika belum ada item yang dipilih
+        value: selectedValue,
+        onChanged: (newValue) {
+          setState(() {
+            selectedValue = newValue;
+          });
+        },
+        items: kelas.map((item) {
+          return DropdownMenuItem(
+            value: item['id'].toString(),
+            child: Text(item['title']), //ubah "id" dengan key atau nama yang ingin dijadikan nilai pada DropdownMenuItem
+          );
+        }).toList(),
+      );
+    }
 
     return Scaffold(
       body: ListView(
