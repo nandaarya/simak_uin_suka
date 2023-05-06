5edb92b95f9c1e78b95457a06b2796e7e29b6264
Nanda Arya Putra <nandaarya404@gmail.com>
2023-05-06 13:50:37 +0700

    fix QRscan

--- lib/view/scan_qr_code.dart
+++ lib/view/scan_qr_code.dart
@@ -125,7 +125,7 @@ class _QRScannerState extends State<QRScanner> {
     //     MediaQuery.of(context).size.height < 400)
     //     ? 150.0
     //     : 300.0;
-    var scanArea = 300.0;
+    var scanArea = 150.0;
     // To ensure the Scanner view is properly sizes after rotation
     // we need to listen for Flutter SizeChanged notification and update controller
     return QRView(
