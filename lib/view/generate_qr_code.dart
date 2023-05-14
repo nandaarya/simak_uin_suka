86ec4a3d0a06a7c89a6172a5b65098ad55a37c94
Nanda Arya Putra <nandaarya404@gmail.com>
2023-05-14 22:11:26 +0700

    fix bottom overflow from keyboard

--- lib/view/generate_qr_code.dart
+++ lib/view/generate_qr_code.dart
@@ -32,104 +32,106 @@ class GenerateQRCodeState extends State<GenerateQRCode> {
         title: const Text('Atur Jadwal Kelas'),
         centerTitle: true,
       ),
-      body: Container(
-        margin: EdgeInsets.all(defaultMargin),
-        padding: EdgeInsets.all(defaultPadding),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text('Pilih Kelas', style: h2,),
-            TextField(
-              controller: kelasController,
-              decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Masukkan Kelas Perkuliahan'),
-            ),
-            const SizedBox(height: 8,),
-            Text('Materi Perkuliahan', style: h2,),
-            TextField(
-              controller: materiController,
-              decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Masukkan Materi Perkuliahan'),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                TextButton(
-                    onPressed: () {
-                      DatePicker.showDateTimePicker(context,
-                          showTitleActions: true,
-                          // Without maxTime
-                          minTime: DateTime.now(), onChanged: (date) {
-                      }, onConfirm: (date) {
-                        setState(() {
-                          startTime = date;
-                        });
-                      }, currentTime: DateTime.now(), locale: LocaleType.id);
+      body: SingleChildScrollView(
+        child: Container(
+          margin: EdgeInsets.all(defaultMargin),
+          padding: EdgeInsets.all(defaultPadding),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text('Pilih Kelas', style: h2,),
+              TextField(
+                controller: kelasController,
+                decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Masukkan Kelas Perkuliahan'),
+              ),
+              const SizedBox(height: 8,),
+              Text('Materi Perkuliahan', style: h2,),
+              TextField(
+                controller: materiController,
+                decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Masukkan Materi Perkuliahan'),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  TextButton(
+                      onPressed: () {
+                        DatePicker.showDateTimePicker(context,
+                            showTitleActions: true,
+                            // Without maxTime
+                            minTime: DateTime.now(), onChanged: (date) {
+                        }, onConfirm: (date) {
+                          setState(() {
+                            startTime = date;
+                          });
+                        }, currentTime: DateTime.now(), locale: LocaleType.id);
+                      },
+                      child: const Text(
+                        'Pilih Waktu Mulai',
+                        style: TextStyle(color: Colors.blue),
+                      )),
+                  const Text('-'),
+                  TextButton(
+                      onPressed: () {
+                        DatePicker.showDateTimePicker(context,
+                            showTitleActions: true,
+                            // Without maxTime
+                            minTime: startTime, onChanged: (date) {
+                            }, onConfirm: (date) {
+                              setState(() {
+                                endTime = date;
+                              });
+                            }, currentTime: DateTime.now(), locale: LocaleType.id);
+                      },
+                      child: const Text(
+                        'Pilih Waktu Selesai',
+                        style: TextStyle(color: Colors.blue),
+                      )),
+                ],
+              ),
+              Text('Waktu mulai: ', style: h3.copyWith(fontSize: 14),),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(DateFormat("EEEE, d MMMM yyyy ", "id_ID").format(startTime), style: h3.copyWith(fontSize: 14),),
+                  Text(DateFormat("hh:mm WIB", "id_ID").format(startTime), style: h3.copyWith(fontSize: 14),),
+                ],
+              ),
+              Text('Waktu selesai: ', style: h3.copyWith(fontSize: 14),),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(DateFormat("EEEE, d MMMM yyyy ", "id_ID").format(endTime), style: h3.copyWith(fontSize: 14),),
+                  Text(DateFormat("hh:mm WIB", "id_ID").format(endTime), style: h3.copyWith(fontSize: 14),),
+                ],
+              ),
+              const SizedBox(height: 8,),
+              Text('Masukkan Ruang Perkuliahan', style: h2,),
+              TextField(
+                controller: ruangController,
+                decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Ruang Perkuliahan'),
+              ),
+              const SizedBox(height: 8,),
+              Center(
+                child: ElevatedButton(
+                    onPressed: () async {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: ((context) =>
+                              QRImage(qrData: materiController.text)),
+                        ),
+                      );
                     },
-                    child: const Text(
-                      'Pilih Waktu Mulai',
-                      style: TextStyle(color: Colors.blue),
-                    )),
-                Text('-'),
-                TextButton(
-                    onPressed: () {
-                      DatePicker.showDateTimePicker(context,
-                          showTitleActions: true,
-                          // Without maxTime
-                          minTime: startTime, onChanged: (date) {
-                          }, onConfirm: (date) {
-                            setState(() {
-                              endTime = date;
-                            });
-                          }, currentTime: DateTime.now(), locale: LocaleType.id);
-                    },
-                    child: const Text(
-                      'Pilih Waktu Selesai',
-                      style: TextStyle(color: Colors.blue),
-                    )),
-              ],
-            ),
-            const Text('Waktu mulai: '),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(DateFormat("EEEE, d MMMM yyyy ", "id_ID").format(startTime)),
-                Text(DateFormat("hh:mm WIB", "id_ID").format(startTime)),
-              ],
-            ),
-            const Text('Waktu selesai: '),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(DateFormat("EEEE, d MMMM yyyy ", "id_ID").format(endTime)),
-                Text(DateFormat("hh:mm WIB", "id_ID").format(endTime)),
-              ],
-            ),
-            const SizedBox(height: 8,),
-            Text('Masukkan Ruang Perkuliahan', style: h2,),
-            TextField(
-              controller: ruangController,
-              decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Ruang Perkuliahan'),
-            ),
-            const SizedBox(height: 8,),
-            Center(
-              child: ElevatedButton(
-                  onPressed: () async {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: ((context) =>
-                            QRImage(qrData: materiController.text)),
-                      ),
-                    );
-                  },
-                  child: const Text('ATUR JADWAL')),
-            ),
-          ],
+                    child: const Text('ATUR JADWAL')),
+              ),
+            ],
+          ),
         ),
       ),
     );
