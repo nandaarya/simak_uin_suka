22eeebfbd2b36e1eba1c283f43383164deab0b2c
Nanda Arya Putra <nandaarya404@gmail.com>
2023-05-15 22:07:57 +0700

    try consuming api to dropdown

--- lib/view/generate_qr_code.dart
+++ lib/view/generate_qr_code.dart
@@ -1,8 +1,29 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:flutter_datetime_picker/flutter_datetime_picker.dart';
 import 'package:intl/intl.dart';
 import '../theme.dart';
 import 'qr_image.dart';
+import 'package:http/http.dart' as http;
+
+Future<List<String>> fetchData() async {
+  final response = await http.get(Uri.parse('https://jsonplaceholder.typicode.com/users'));
+
+  // if (response.statusCode == 200) {
+  //   final List<dynamic> data = json.decode(response.body);
+  //   return List<String>.from(data);
+  // } else {
+  //   throw Exception('Failed to fetch data from API');
+  // }
+
+  if (response.statusCode == 200) {
+    final jsonResponse = json.decode(response.body);
+    final List<dynamic> data = jsonResponse['name']; // Ganti 'key' dengan key yang ingin Anda ambil
+    return List<String>.from(data);
+  } else {
+    throw Exception('Failed to load data');
+  }
+}
 
 class GenerateQRCode extends StatefulWidget {
   const GenerateQRCode({super.key});
@@ -17,6 +38,18 @@ class GenerateQRCodeState extends State<GenerateQRCode> {
   TextEditingController kelasController = TextEditingController();
   DateTime startTime = DateTime.now();
   DateTime endTime = DateTime.now();
+  String? selectedKelas;
+  List<String> dropdownKelas = [];
+
+  @override
+  void initState() {
+    super.initState();
+    fetchData().then((data) {
+      setState(() {
+        dropdownKelas = data;
+      });
+    });
+  }
 
   @override
   void dispose() {
@@ -39,7 +72,10 @@ class GenerateQRCodeState extends State<GenerateQRCode> {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Text('Pilih Kelas', style: h2,),
+              Text(
+                'Pilih Kelas',
+                style: h2,
+              ),
               TextField(
                 controller: kelasController,
                 decoration: const InputDecoration(
@@ -47,7 +83,25 @@ class GenerateQRCodeState extends State<GenerateQRCode> {
                     labelText: 'Masukkan Kelas Perkuliahan'),
               ),
               const SizedBox(height: 8,),
-              Text('Materi Perkuliahan', style: h2,),
+              DropdownButton<String>(
+                value: selectedKelas,
+                items: dropdownKelas.map((String item) {
+                  return DropdownMenuItem<String>(
+                    value: item,
+                    child: Text(item),
+                  );
+                }).toList(),
+                onChanged: (String? newValue) {
+                  setState(() {
+                    selectedKelas = newValue;
+                  });
+                },
+              ),
+              const SizedBox(height: 8,),
+              Text(
+                'Materi Perkuliahan',
+                style: h2,
+              ),
               TextField(
                 controller: materiController,
                 decoration: const InputDecoration(
@@ -62,8 +116,8 @@ class GenerateQRCodeState extends State<GenerateQRCode> {
                         DatePicker.showDateTimePicker(context,
                             showTitleActions: true,
                             // Without maxTime
-                            minTime: DateTime.now(), onChanged: (date) {
-                        }, onConfirm: (date) {
+                            minTime: DateTime.now(),
+                            onChanged: (date) {}, onConfirm: (date) {
                           setState(() {
                             startTime = date;
                           });
@@ -79,12 +133,12 @@ class GenerateQRCodeState extends State<GenerateQRCode> {
                         DatePicker.showDateTimePicker(context,
                             showTitleActions: true,
                             // Without maxTime
-                            minTime: DateTime.now(), onChanged: (date) {
-                            }, onConfirm: (date) {
-                              setState(() {
-                                endTime = date;
-                              });
-                            }, currentTime: DateTime.now(), locale: LocaleType.id);
+                            minTime: DateTime.now(),
+                            onChanged: (date) {}, onConfirm: (date) {
+                          setState(() {
+                            endTime = date;
+                          });
+                        }, currentTime: DateTime.now(), locale: LocaleType.id);
                       },
                       child: const Text(
                         'Pilih Waktu Selesai',
@@ -92,32 +146,59 @@ class GenerateQRCodeState extends State<GenerateQRCode> {
                       )),
                 ],
               ),
-              Text('Waktu mulai: ', style: h3.copyWith(fontSize: 14),),
+              Text(
+                'Waktu mulai: ',
+                style: h3.copyWith(fontSize: 14),
+              ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Text(DateFormat("EEEE, d MMMM yyyy ", "id_ID").format(startTime), style: h3.copyWith(fontSize: 14),),
-                  Text(DateFormat("HH:mm WIB", "id_ID").format(startTime), style: h3.copyWith(fontSize: 14),),
+                  Text(
+                    DateFormat("EEEE, d MMMM yyyy ", "id_ID").format(startTime),
+                    style: h3.copyWith(fontSize: 14),
+                  ),
+                  Text(
+                    DateFormat("HH:mm WIB", "id_ID").format(startTime),
+                    style: h3.copyWith(fontSize: 14),
+                  ),
                 ],
               ),
-              const SizedBox(height: 8,),
-              Text('Waktu selesai: ', style: h3.copyWith(fontSize: 14),),
+              const SizedBox(
+                height: 8,
+              ),
+              Text(
+                'Waktu selesai: ',
+                style: h3.copyWith(fontSize: 14),
+              ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Text(DateFormat("EEEE, d MMMM yyyy ", "id_ID").format(endTime), style: h3.copyWith(fontSize: 14),),
-                  Text(DateFormat("HH:mm WIB", "id_ID").format(endTime), style: h3.copyWith(fontSize: 14),),
+                  Text(
+                    DateFormat("EEEE, d MMMM yyyy ", "id_ID").format(endTime),
+                    style: h3.copyWith(fontSize: 14),
+                  ),
+                  Text(
+                    DateFormat("HH:mm WIB", "id_ID").format(endTime),
+                    style: h3.copyWith(fontSize: 14),
+                  ),
                 ],
               ),
-              const SizedBox(height: 8,),
-              Text('Masukkan Ruang Perkuliahan', style: h2,),
+              const SizedBox(
+                height: 8,
+              ),
+              Text(
+                'Masukkan Ruang Perkuliahan',
+                style: h2,
+              ),
               TextField(
                 controller: ruangController,
                 decoration: const InputDecoration(
                     border: OutlineInputBorder(),
                     labelText: 'Ruang Perkuliahan'),
               ),
-              const SizedBox(height: 8,),
+              const SizedBox(
+                height: 8,
+              ),
               Center(
                 child: ElevatedButton(
                     onPressed: () async {
