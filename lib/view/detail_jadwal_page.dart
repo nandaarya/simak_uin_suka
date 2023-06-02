7d8d3f18aaaf2ae03a0bb4f1fb7afe54d381ac39
Nanda Arya Putra <nandaarya404@gmail.com>
2023-06-02 23:02:06 +0700

    add detail jadwal page

--- lib/view/detail_jadwal_page.dart
+++ lib/view/detail_jadwal_page.dart
@@ -1,10 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:qr_flutter/qr_flutter.dart';
 
 class DetailJadwalPage extends StatelessWidget {
-  const DetailJadwalPage({Key? key}) : super(key: key);
+  final String classCode,
+      className,
+      lecturer,
+      material,
+      startedAt,
+      finishAt,
+      room;
+  const DetailJadwalPage(
+      {super.key,
+      required this.classCode,
+      required this.className,
+      required this.lecturer,
+      required this.material,
+      required this.startedAt,
+      required this.finishAt,
+      required this.room});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Column(
+      children: [
+        QrImage(
+          data: classCode,
+          size: 300,
+        ),
+        Text(className),
+        Text(lecturer),
+        Text(material),
+        Text(startedAt),
+        Text(finishAt),
+        Text(room)
+      ],
+    );
   }
 }
