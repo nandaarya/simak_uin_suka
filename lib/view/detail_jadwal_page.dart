e92e33dcc5cb0b392498b09d9a8b3fd974dd3998
Nanda Arya Putra <nandaarya404@gmail.com>
2023-06-03 16:36:58 +0700

    fix layout detail jadwal page

--- lib/view/detail_jadwal_page.dart
+++ lib/view/detail_jadwal_page.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:qr_flutter/qr_flutter.dart';
 
+import '../theme.dart';
+import 'main_page.dart';
+
 class DetailJadwalPage extends StatelessWidget {
   final String classCode,
       className,
@@ -26,19 +29,125 @@ class DetailJadwalPage extends StatelessWidget {
         title: const Text('Detail Jadwal'),
         centerTitle: true,
       ),
-      body: Column(
-        children: [
-          QrImage(
-            data: classCode,
-            size: 300,
-          ),
-          Text(className),
-          Text(lecturer),
-          Text(material),
-          Text(startedAt),
-          Text(finishAt),
-          Text(room)
-        ],
+      body: Container(
+        margin: EdgeInsets.all(defaultMargin),
+        padding: EdgeInsets.all(defaultPadding),
+        child: Column(
+          children: [
+            Text('QR Presensi', style: h2,),
+            const SizedBox(height: 16,),
+            QrImage(
+              data: classCode,
+              size: 300,
+            ),
+            const SizedBox(height: 16,),
+            Column(
+              children: [
+                Text(
+                  className,
+                  style: h3b,
+                ),
+                const SizedBox(
+                  height: 8,
+                ),
+                Row(
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Dosen',
+                          style: h3,
+                        ),
+                        Text(
+                          'Materi',
+                          style: h3,
+                        ),
+                        Text(
+                          'Mulai',
+                          style: h3,
+                        ),
+                        Text(
+                          'Selesai',
+                          style: h3,
+                        ),
+                        Text(
+                          'Ruang',
+                          style: h3,
+                        ),
+                      ],
+                    ),
+                    const SizedBox(
+                      width: 8,
+                    ),
+                    Column(
+                      children: [
+                        Text(
+                          ':',
+                          style: h3,
+                        ),
+                        Text(
+                          ':',
+                          style: h3,
+                        ),
+                        Text(
+                          ':',
+                          style: h3,
+                        ),
+                        Text(
+                          ':',
+                          style: h3,
+                        ),
+                        Text(
+                          ':',
+                          style: h3,
+                        ),
+                      ],
+                    ),
+                    const SizedBox(
+                      width: 8,
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          lecturer,
+                          style: h3,
+                        ),
+                        Text(
+                          material,
+                          style: h3,
+                        ),
+                        Text(
+                          startedAt,
+                          style: h3,
+                        ),
+                        Text(
+                          finishAt,
+                          style: h3,
+                        ),
+                        Text(
+                          room,
+                          style: h3,
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            const SizedBox(height: 16,),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const MainPage()),
+                );
+              },
+              child: const Text('Kembali ke Beranda'),
+            ),
+          ],
+        ),
       ),
     );
   }
