d4dbaf935dc7af2a3c373074f943c2b9ffa88999
Nanda Arya Putra <nandaarya404@gmail.com>
2023-06-04 22:39:31 +0700

    fix login logic

--- lib/view/sign_in_page.dart
+++ lib/view/sign_in_page.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:flutter_login/flutter_login.dart';
@@ -15,17 +17,36 @@ class SignInPage extends StatelessWidget {
 
   Duration get loginTime => const Duration(milliseconds: 2250);
 
-  Future<String?> _authUser(LoginData data) {
+  Future<String?> _authUser(LoginData data) async {
     debugPrint('Name: ${data.name}, Password: ${data.password}');
-    return Future.delayed(loginTime).then((_) {
-      if (!users.containsKey(data.name)) {
-        return 'User not exists';
+    try {
+      var url =
+          Uri.parse('https://simak-back-end.cyclic.app/api/users/' + 'login');
+      var requestBody = json.encode({
+        "username": data.name,
+        "password": data.password,
+      });
+      var response = await http.post(url,
+          headers: <String, String>{
+            'Content-Type': 'application/json; charset=UTF-8',
+          },
+          body: requestBody);
+      var jsonData = json.decode(response.body);
+      if (response.statusCode == 200) {
+        // Proses response atau lakukan operasi lain setelah POST berhasil
+        // var classCode = jsonData['data']['classCode'];
+        print('Berhasil Login');
+        print(jsonData);
+        return null;
+      } else {
+        print('POST request gagal dengan status code: ${response.statusCode}');
+        return jsonData['message'];
       }
-      if (users[data.name] != data.password) {
-        return 'Password does not match';
-      }
-      return null;
-    });
+    } catch (e) {
+      print('Something went wrong while login');
+      print(e);
+    }
+    return null;
   }
 
   Future<String?> _signupUser(SignupData data) {
@@ -60,9 +81,7 @@ class SignInPage extends StatelessWidget {
       userValidator: _usernameValidator,
       title: 'SIMAK UIN SUKA',
       logo: const AssetImage('assets/images/logo_uin.png'),
-      messages: LoginMessages(
-        userHint: 'NIM / NIP'
-      ),
+      messages: LoginMessages(userHint: 'NIM / NIP'),
       theme: LoginTheme(
         titleStyle: h1,
         headerMargin: defaultMargin,
